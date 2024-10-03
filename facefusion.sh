f5c95356bca37b971c86139de2c0374de9bd9c65
singulainthony <singulainthony@proton.me>
2024-10-03 18:19:05 -0400

    Update facefusion.sh

--- facefusion.sh
+++ facefusion.sh
@@ -4,4 +4,4 @@ echo "Launching FaceFusion... Reload webpage in browser in 10-15 seconds"
 
 python3 -m webbrowser "http://127.0.0.1:7860/"
 
-python3 /root/vast-facefusion/facefusion.py run
+python3 /root/facefusion/facefusion.py run
