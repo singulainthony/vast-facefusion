e54a9684284d2809366d702756629bb884d7cd67
singulainthony <singulainthony@proton.me>
2024-10-03 15:56:25 -0400

    Add files via upload

---
+++ FFScript.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+echo Setting Platform
+
+apt --assume-yes -y install git-all &&
+apt --assume-yes -y  install curl &&
+apt --assume-yes -y  install ffmpeg &&
+apt-get --assume-yes -y install mesa-va-drivers &&
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh &&
+bash ~/miniconda.sh -b -p $HOME/miniconda &&
+eval "$(/$HOME/miniconda/bin/conda shell.zsh hook)" &&
+
+conda init --all &&
+conda create --name facefusion python=3.10  -y &&
+conda activate facefusion &&
+
+echo Runtime Install
+
+conda install -y conda-forge::cuda-runtime=12.4.1 conda-forge::cudnn=9.2.1.18 &&
+pip install tensorrt==10.4.0 -q --extra-index-url https://pypi.nvidia.com &&
+
+
+echo Installing FaceFusion
+
+cd /root/ &&
+git clone https://github.com/facefusion/facefusion.git &&
+
+cd /root/facefusion/ &&
+python3 install.py --onnxruntime cuda &&
+conda deactivate && conda deactivate &&
+conda activate facefusion &&
+
+echo Run FF
+
+
+python3 facefusion.py run
+
---
+++ facefusion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+python3 -m webbrowser "http://127.0.0.1:7860/"
